5e0c6f4e9f711809e61abd7ceb8e0d00b8780029
harry fenton <harry.j.fenton@gmail.com>
2026-04-20 00:21:57 +0100

    ram reading and writing

--- src/ram.py
+++ src/ram.py
@@ -1,9 +1,14 @@
 class Ram:
+    
     def __init__(self):
-        pass
+        self.memory = [0] * 8000
 
     def get_value_at_address(self, address):
-        pass
+        return self.memory[address]
 
     def set_value_at_address(self, address, value):
-        pass
+        self.memory[address] = value
+
+##ram = Ram()
+##ram.set_value_at_address(40,7)
+##print(ram.get_value_at_address(40))

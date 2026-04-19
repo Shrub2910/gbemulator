afcbcbfb19d702ef8b25dc627baee6d66f17f142
harry fenton <harry.j.fenton@gmail.com>
2026-04-20 00:48:52 +0100

    rom loader and getting rom byte stream in main

--- src/rom_loader.py
+++ src/rom_loader.py
@@ -1,2 +1,4 @@
-def load_rom_byte_stream():
-    pass
+def load_rom_byte_stream(rom_path):
+    with open(rom_path,"rb") as file:
+        byte_stream = file.read()
+        return io.BytesIO(byte_stream)

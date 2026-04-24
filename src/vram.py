0e672fda2530c8c40252a0d4ae9962b1ba72b5e7
harry fenton <harry.j.fenton@gmail.com>
2026-04-24 20:45:27 +0100

    fixed data sizes

--- src/vram.py
+++ src/vram.py
@@ -5,7 +5,7 @@ class VRam(Memory):
     U_BOUNDARY = 0x9FFF
 
     def __init__(self):
-        self.data = [0] * 0x4000
+        self.data = [0] * 0x2000
     
     def get_value_at_address(self,address):
         return self.data[address-L_boundary]

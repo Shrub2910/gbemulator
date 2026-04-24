b4607b5732a899e7b3e2b90bfa0032cd762ff48b
orangehaz12 <80492856+orangehaz12@users.noreply.github.com>
2026-04-24 20:48:36 +0100

    reworked memory classes (#6)
    * reworked memory classes
    
    * fixed data sizes

--- src/vram.py
+++ src/vram.py
@@ -1,12 +1,11 @@
 from memory import Memory
 
-
-class Ram(Memory):
-    L_BOUNDARY = 0xC000
-    U_BOUNDARY = 0xDFFF
+class VRam(Memory):
+    L_BOUNDARY = 0x8000
+    U_BOUNDARY = 0x9FFF
 
     def __init__(self):
-        self.data = [0]*0x2000
+        self.data = [0] * 0x2000
     
     def get_value_at_address(self,address):
         return self.data[address-L_boundary]

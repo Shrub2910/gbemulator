a5f8969cc30620df2f8e927531f9e5d40720e6fd
Isaac McAllister <102828786+Shrub2910@users.noreply.github.com>
2026-04-24 17:35:56 +0100

    Feature CPU changes (#4)
    * add read write address to cpu
    
    * fix cannot write to ram

--- src/cpu.py
+++ src/cpu.py
@@ -1,6 +1,16 @@
 class Cpu:
-    def __init__(self, program_data):
-        self.program_data = program_data
+    ROM_BOUNDARY = 0x8000
+    VRAM_BOUNDARY = 0xA000
+    EXTERNAL_BOUNDARY = 0xC000
+    RAM_BOUNDARY = 0xE000
+    ECHO_BOUNDARY = 0xFE00
+    OAM_BOUNDARY = 0xFEA0
+    UNUSED_BOUNDARY = 0xFF00
+    IO_REGISTER_BOUNDARY = 0xFF80
+    HIGH_RAM_BOUNDARY = 0xFFFF
+    INTERUPT_REGISTER_LOCATION = 0xFFFF
+
+    def __init__(self, rom, ram, vram):
         self.pc = 0x0000
         self.sp = 0xFFFE
         self.A = 0x00
@@ -11,19 +21,67 @@ class Cpu:
         self.F = 0x00
         self.H = 0x00
         self.L = 0x00
-        self.m_cycles = 0
+        self.t_cycles = 0
+        self.rom = rom
+        self.ram = ram
+        self.vram = vram
 
     def get_next_byte(self):
         byte = self.program_data[self.pc]
         self.pc += 1
         return byte
 
+    def read_address(self, address):
+        if address < Cpu.ROM_BOUNDARY:
+            return self.rom.get_value_at_address(address)
+        elif address < Cpu.VRAM_BOUNDARY:
+            return self.vram.get_value_at_address(address)
+        elif address < Cpu.EXTERNAL_BOUNDARY:
+            pass
+        elif address < Cpu.RAM_BOUNDARY:
+            return self.ram.get_value_at_address(address)
+        elif address < Cpu.ECHO_BOUNDARY:
+            pass
+        elif address < Cpu.OAM_BOUNDARY:
+            pass
+        elif address < Cpu.UNUSED_BOUNDARY:
+            pass
+        elif address < Cpu.IO_REGISTER_BOUNDARY:
+            pass
+        elif address < Cpu.HIGH_RAM_BOUNDARY:
+            pass
+        elif address == Cpu.INTERUPT_REGISTER_LOCATION:
+            pass
+        self.m_cycle()
+
+    def write_address(self, address, value):
+        if address < Cpu.ROM_BOUNDARY:
+            raise Exception("Cannot write to rom address")
+        elif address < Cpu.EXTERNAL_BOUNDARY:
+            pass
+        elif address < Cpu.RAM_BOUNDARY:
+            self.ram.get_value_at_address(address, value)
+        elif address < Cpu.ECHO_BOUNDARY:
+            pass
+        elif address < Cpu.OAM_BOUNDARY:
+            pass
+        elif address < Cpu.UNUSED_BOUNDARY:
+            pass
+        elif address < Cpu.IO_REGISTER_BOUNDARY:
+            pass
+        elif address < Cpu.HIGH_RAM_BOUNDARY:
+            pass
+        elif address == Cpu.INTERUPT_REGISTER_LOCATION:
+            pass
+        self.m_cycle()
+
+    def m_cycle(self):
+        for i in range(0, 4):
+            self.t_cycles += 1
+
     def run(self):
         while True:
             opcode = self.get_next_byte()
-            self.m_cycles += self.step(opcode)
-
-    def step(self, opcode):
-        match opcode:
-            case 0x00:
-                return 1
+            match opcode:
+                case 0x00:
+                    pass
